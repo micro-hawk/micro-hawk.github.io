f4a58a26a1512b177a080f60e2f35ee7dfe29b56
micro-hawk <dasvikas0071@gmail.com>
2022-07-21 19:42:44 +0530

    refactored

--- js/cursor.js
+++
@@ -1,4 +0,0 @@
-const cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY-10)+"px; left: "+(e.pageX-10)+"px;" )
-})
